a57b7125d19167f43637624719459f4925b4eeda
TheSEP42 <thesep42@googlemail.com>
2017-09-11 02:32:41 +0100

    Fix to Login and basic welcome page
    - Fix issue with login trying to use username when requesting email.
    - Add basic welcome splash page after login.

--- BasicWebApp/Controllers/AccountController.cs
+++ BasicWebApp/Controllers/AccountController.cs
@@ -75,11 +75,11 @@ namespace BasicWebApp.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    return RedirectToAction("Welcome");
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
@@ -90,6 +90,14 @@ namespace BasicWebApp.Controllers
                     return View(model);
             }
         }
+
+        // GET: /Account/Welcome
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+
+
 
         //
         // GET: /Account/VerifyCode
